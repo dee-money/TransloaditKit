bfb14710fab7377ef8352952ab910958bead654b
Rupendra <rupendra@example.com>
2023-06-28 16:02:20 +0700

    Swift package manager support added for 2.0*.

---
+++ Tests/TransloaditKitTests/TransloaditKitTests.swift
@@ -0,0 +1,7 @@
+import XCTest
+import TransloaditKit // ⚠️ WARNING: We are not performing a testable import here. We want to test the real public API. By doing so, we'll know very quicklly if the public API is broken. Which is very important to prevent.
+
+final class TransloaditKitTests: XCTestCase {
+    func testExample() {
+    }
+}
